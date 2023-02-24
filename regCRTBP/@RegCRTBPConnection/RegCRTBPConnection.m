fbb4d45f50c6ae3672c689b4a3c3c90a0c2568bb
Shane <shane.kepley@rutgers.edu>
2023-02-24 12:44:59 +0100

    Added a class for Connections

---
+++ regCRTBP/@RegCRTBPConnection/RegCRTBPConnection.m
@@ -0,0 +1,72 @@
+classdef RegCRTBPConnection < handle
+    %REGCRTBPCONNECTION - One line description of the class (H1 line)
+    %
+    %   REGCRTBPCONNECTION() - Connection class for the GCS algorithm for the CRTBP with Levi-Cevita regularization.
+    %
+    %   REGCRTBPCONNECTION constructor syntax:
+    %       RegCRTBPConnectionObj = $NAME()
+    %
+    %   REGCRTBPCONNECTION properties:
+    %       Property 1 - description
+    %       Property 2 - description
+    %
+    %   REGCRTBPCONNECTION methods:
+    %       Method 1 - description
+    %       Method 2 - description
+    %
+    %   Examples:
+    %       Line 1 of example
+    %       Line 2 of example
+    %       Line 3 of example
+    %
+    %   Subfunctions: none
+    %   Classes required: none
+    %   Other m-files required: none
+    %   MAT-files required: none
+    
+    %   Author: Shane Kepley
+    %   email: s.kepley@vu.nl
+    %   Date: 23-Feb-2023;
+    %
+    %   ToDo:
+    %   item1 -
+    %   item2 -
+    
+    
+    %% ---------------------------------------- Properties ----------------------------------------
+    properties
+        StableChart
+        UnstableChart
+        LocalIntersection  % [stableSpace, stableTime, unstableSpace] coordinates for the interesection in the local coordinate system which is a subset of D^d
+        GlobalIntersection  % [stableSpace, stableTime, unstableSpace, unstableTime] = global coordinates for the intersection:
+        ConnectionTime  % time of flight in physical time between source manifold and target manifold
+        LocalMaps  % [stableLocalMap, unstableLocalMap]
+        TrueOrbit  % heuristics for deciding if the orbit is a connection or pseudo connection
+        Parameter % mass ratio of the small primary
+        
+        
+    end % end properties
+    
+    
+    %% ---------------------------------------- Methods ----------------------------------------
+    methods
+        function obj = RegCRTBPConnection(connectionIntersectionData, stableLocalMap, unstableLocalMap)
+            %REGCRTBPCONNECTION - class constructor
+            warning('OFF', 'Chart:eval');  % turn off Chart.eval warning messages
+            warning('OFF', 'Chart:local2global');  % turn off Chart.eval warning messages
+            disp(connectionIntersectionData)
+            obj.StableChart = connectionIntersectionData{1};
+            obj.UnstableChart = connectionIntersectionData{2};
+            obj.LocalIntersection = connectionIntersectionData{3};  % [stableSpace, stableRegTime, unstableSpace]
+            
+            localStablePhysTime = taylorregtime(obj.StableChart, obj.LocalIntersection(2));
+            
+            obj.GlobalIntersection = [obj.StableChart.local2global([obj.LocalIntersection(1), localStablePhysTime]),...
+                obj.UnstableChart.local2global([obj.LocalIntersection(3), 0])]; % [stableSpace, stableTime, unstableSpace, unstableTime]
+            obj.ConnectionTime = obj.GlobalIntersection(4) - obj.GlobalIntersection(2);  % time of flight in physical time
+            obj.TrueOrbit = connectionIntersectionData{4};
+            obj.LocalMaps = {stableLocalMap, unstableLocalMap};
+            obj.Parameter = obj.StableChart.Parameter;
+        end % end class constructor
+    end % end methods
+end
---
+++ regCRTBP/@RegCRTBPConnection/plot.m
@@ -0,0 +1,46 @@
+function [output1,output2] = plot(obj, nNode, varargin)
+%PLOT - One line description of what the function or script performs (H1 line)
+%
+%   Syntax:
+%       output = PLOT(input)
+%    
+%   Inputs:
+%       input1 - Description
+%       input2 - Description
+%
+%   Outputs:
+%       output1 - Description
+%       output2 - Description
+%
+%   Subfunctions: none
+%   Classes required: none
+%   Other m-files required: none
+%   MAT-files required: none
+
+%   Author: Shane Kepley
+%   email: s.kepley@vu.nl
+%   Date: 23-Feb-2023; 
+
+sampleData = obj.sample(nNode);
+mu = obj.Parameter;
+
+figure
+hold on
+for iStrand = 1:length(sampleData.Orbit)
+    switch sampleData.RegVector(iStrand)
+        case 0
+            jSeg = sampleData.Orbit{iStrand};
+            plot(jSeg(1,:), jSeg(3,:), 'g', 'LineWidth', 1, varargin{:})
+            
+        case 1
+            jSeg = CRTBP2reg(sampleData.Orbit{iStrand}.', mu, -1).';
+            plot(jSeg(1,:), jSeg(3,:), 'r', 'LineWidth', 1, varargin{:})
+            
+        case 2
+            jSeg = CRTBP2reg(sampleData.Orbit{iStrand}.', mu, -2).';
+            plot(jSeg(1,:), jSeg(3,:), 'b', 'LineWidth', 1, varargin{:})
+    end
+end
+
+end % end plot
+
